f3233c4aea35a43fd636c7d60ae35d0f672908a0
suyongeum <suyongeum@gmail.com>
2018-09-16 10:41:45 +0900

    Feature Extraction and SVM complete. Begin working on feature plot based on PCA

--- SVM/svm_model/featureExtraction.py
+++ SVM/svm_model/featureExtraction.py
@@ -5,7 +5,10 @@ import urllib.request
 from bs4 import BeautifulSoup
 import math
 import random
-from sklearn.externals import joblib
+import dill
+import re
+import time
+import difflib
 
 class FeatureExtraction:
 
@@ -18,18 +21,57 @@ class FeatureExtraction:
         self.google_log = 0
         self.freq_guten = 0
         self.pos = 0
-        features = pd.read_csv('./featureExtraction_for_training/total_extracted_features.csv', keep_default_na=False)
-        self.words = features.loc[:, 'word']
-        self.guten_all = features.loc[:, 'gutenfq']
+        self.close_word = ''
+        self.features = pd.read_csv('./featureExtraction_for_training/total_extracted_features.csv', keep_default_na=False)
+        self.words = self.features.loc[:, 'word']
 
     def run_prediction(self, word):
         # obtain features of the given word
+        self.search_similar_word_v1(word)  # set self.close_word, self.close_word_index
         self.fvrlen(word)
-        self.fweblio(word)
-        self.fgoogle_log(word)
-        #self.ffreq_guten(word)
+        self.fweblio_mod(word)
+        self.fgoogle_log_mod(word)
         self.ffreq_guten_mod(word)
-        self.fpos(word)
+        self.fpos_mod(word)
+
+        # self.search_similar_word(word)  # set self.close_word, self.close_word_index
+        # self.fvrlen(word)
+        # self.fweblio(word)
+        # self.fgoogle_log(word)
+        # self.ffreq_guten(word)
+        # self.fpos(word)
+
+    def search_similar_word(self, word):
+        min = 100
+        index = 0
+        for word_ in self.words:
+            distance = nltk.edit_distance(word_, word)
+            if distance < min:
+                min = distance
+                self.close_word = word_
+                self.close_word_index = index
+            index = index + 1
+        print('search:', self.close_word)
+
+    def search_similar_word_v1(self, word):
+        found = difflib.get_close_matches(word, self.words)
+        print("3 similar words:", found)
+        if found:
+            for i in found:
+                if i[0] == word[0]:
+                    self.close_word = i
+                    break
+            for i in found:
+                if re.search(i, word+'[a-z]'):
+                    self.close_word = i
+                    break
+            if not self.close_word:
+                self.close_word = found[0]
+        else:
+            self.close_word = 'condolence'
+        print('----------------------------------------------')
+        print('original word:', word, '-- diff search:', self.close_word)
+        print('----------------------------------------------')
 
     def fvrlen(self, word):
         self.len = len(word)
@@ -48,17 +90,35 @@ class FeatureExtraction:
         bsObj = BeautifulSoup(f.read(), "html.parser")
         learning_level = bsObj.find('span', class_='learning-level-content')
         f.close()
-        self.weblio = learning_level.text
+
+        if learning_level:
+            self.weblio = learning_level.text
+        else:
+            self.weblio = 'encouragement'
+
+    def fweblio_mod(self, word):
+        if (self.features).iloc[(self.words).tolist().index(self.close_word)]['weblio']:
+            self.weblio = (self.features).iloc[(self.words).tolist().index(self.close_word)]['weblio']
+        else:
+            self.weblio = 'encouragement'
+
+        #print("weblio:", self.weblio)
 
     def fgoogle_log(self, word):
         req = urllib.request.Request('https://www.google.com/search?q=' + word,
-                                     headers={'User-Agent': 'PracticalMachineLearning' + str(random.randint(0, 500))})
+                                     headers={'User-Agent': 'PracticalMachineLearning' + str(random.randint(0, 500)),
+                                              'Disallow': '/*data*$'})
         f = urllib.request.urlopen(req)
         bsObj = BeautifulSoup(f.read(), "html.parser")
         site_num_ori = bsObj.find('div', id='resultStats').string
-        site_num = int(site_num_ori.replace('約 ', '').replace(',', '').replace(' 件', ''))
+        site_num = ''.join(re.findall(r'\d+', site_num_ori))
         f.close()
-        self.google_log =math.log10(site_num)
+        self.google_log =math.log10(float(site_num))
+
+    def fgoogle_log_mod(self, word):
+        self.google_log = (self.features).iloc[(self.words).tolist().index(self.close_word)]['googlelog']
+        self.google_log =math.log10(float(self.google_log))
+        #print("google_log:", self.google_log)
 
     def ffreq_guten(self, word):
         datasets = nltk.corpus.gutenberg.fileids()
@@ -69,14 +129,8 @@ class FeatureExtraction:
         self.freq_guten = total_freq
 
     def ffreq_guten_mod(self, word):
-        min=100
-        for word_, guten_freq in zip(self.words, self.guten_all):
-            distance = nltk.edit_distance(word_, word)
-            if distance < min:
-                min = distance
-                close_word = word_
-                self.freq_guten = guten_freq
-        #print(close_word, self.freq_guten)
+        self.freq_guten = (self.features).iloc[(self.words).tolist().index(self.close_word)]['gutenfq']
+        #print("guten_frq:", self.freq_guten)
 
     def fpos(self, word):
         a = nltk.pos_tag([word])
@@ -106,15 +160,49 @@ class FeatureExtraction:
                 return
         self.pos = 10
 
-    def makeFeaturepickle(self):
-        # save the model to disk
-        dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-        joblib.dump(self, os.path.join(dir_path, 'PML/pickled/features.sav'))
+    def fpos_mod(self, word):
+        self.pos = (self.features).iloc[(self.words).tolist().index(self.close_word)]['pos']
+        #print("pos:", self.freq_guten)
+
+    # def makeFeaturepickle(self):
+    #     # save the model to disk
+    #     dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    #     joblib.dump(self, os.path.join(dir_path, 'PML/pickled/features.sav'))
 
 if __name__ == '__main__' :
 
+    # Creation of a pickle
     svmFeatures = FeatureExtraction()
-    svmFeatures.makeFeaturepickle()
+    dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    dill.settings['recurse'] = True # to access a member function
+    dill.dump(svmFeatures, open(os.path.join(dir_path, 'PML\\pickled\\features.dill'), "wb"))
+
+    # Testing
+    word = 'reactively'
+
+    start = time.clock()
+    svmFeatures.search_similar_word_v1(word)
+    print("------search similar word time v1:", time.clock() - start)
+
+    start = time.clock()
+    svmFeatures.fvrlen(word)
+    print("------fvlen time:", time.clock() - start)
+
+    start = time.clock()
+    svmFeatures.fweblio_mod(word)
+    print("------weblio time:", time.clock() - start)
+
+    start = time.clock()
+    svmFeatures.fgoogle_log_mod(word)
+    print("------fgoodle time:", time.clock() - start)
+
+    start = time.clock()
+    svmFeatures.ffreq_guten_mod(word)
+    print("------guten time:", time.clock() - start)
+
+    start = time.clock()
+    svmFeatures.fpos_mod(word)
+    print("------pos time:", time.clock() - start)
 
 
 
--- SVM/views.py
+++ SVM/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from PML.nlp.Lemmatiser import Lemmatiser
 from PCA.models import Word
 import re
+import dill
+import time
 
 from sklearn.externals import joblib
 from SVM.svm_model.featureExtraction import FeatureExtraction
@@ -42,36 +44,38 @@ def check(request):
         if obj.count() == 0:
             # if the word includes a special character or number
             # not consider it as a word
-            if re.match(r'^\w+$', word):
+            if (re.search('[0-9]', word) == None)and(re.search('[a-z]', word) != None):
                 # Need to create SVM function
-                estimated_difficulty = 1;
-                not_found.append([word, estimated_difficulty])
-                print("not found: ", word)
+                not_found.append([word, 0])
                 # SVM is used to return an inferred difficulty.
         else:
             # if the word includes a special character or number
             # not consider it as a word
-            if re.match(r'^\w+$', word):
-                # making [ [word, 1], [word, 3], ... ]
-                found.append([obj.first().word, obj.first().difficulty])
-                print("found: ", word)
+            # making [ [word, 1], [word, 3], ... ]
+            found.append([obj.first().word, obj.first().difficulty])
+            #print("found: ", word)
     ########################################################################
     # 5. If does not exist, classify using SVM result.
 
     # 5-1. Feature extraction and prediction
-    featureModel = joblib.load('PML/pickled/features.sav')
-    svmModel = joblib.load('PML/pickled/svm_len_vrlen_googlelog_pos_weblio_gutenfq.sav')
+    start = time.clock()
+    featureModel = dill.load(open(r"PML/pickled/features.dill", "rb"))
+    #svmModel = joblib.load('PML/pickled/svm_len_vrlen_googlelog_pos_weblio_gutenfq.sav')
+    #svmModel = joblib.load('PML/pickled/svm_len_vrlen_googlelog_pos_weblio.sav')
+    svmModel = joblib.load('PML/pickled/svm_len_vrlen_googlelog_pos.sav')
+    print("Loading time:", time.clock() - start)
 
     not_found_ = []
     for item in not_found:
-
-        print(item[0], item[1])
 
         word = item[0]
         difficulty = item[1]
 
         # feature extraction
+        start = time.clock()
         featureModel.run_prediction(word)
+        print("feature extraction time:", time.clock() - start)
+
         f1 = featureModel.len
         f2 = featureModel.vrlen
         f3 = featureModel.google_log
@@ -79,12 +83,31 @@ def check(request):
         f5 = featureModel.weblio
         f6 = featureModel.freq_guten
 
-        # prediction
-        difficulty = svmModel.predict([[f1, f2, f3, f4, f5, f6]])[0]
-        not_found_.append([word, difficulty])
+        print("len:", f1)
+        print("vrlen: %.2f" % f2)
+        print("google_log: %.2f" % f3)
+        print("pos:", f4)
+        print("weblio:", f5)
+        print("freq_guten:", f6)
 
+        # prediction
+        start = time.clock()
+        #difficulty = svmModel.predict([[f1, f2, f3, f4, f5, f6]])[0]
+        #difficulty = svmModel.predict([[f1, f2, f3, f4])[0]
+        difficulty = svmModel.predict([[f1, f2, f3, f4]])[0]
+        print("svm running time:", time.clock() - start)
+        print('----------------------------------------------')
+        print("Difficulty Level is: ", difficulty)
+        print('----------------------------------------------')
+
+        not_found_.append([word, int(difficulty)])
+
+    # if not found and not_found_:
+    #     pass
+    # else:
     response = {'not_found': not_found_, 'found': found}
     return JsonResponse(response)
+
 
 
 
